2abcedd75319ecb6ef1fa4db37d6636e799f2438
y dang <dangvannhuy1106@gmail.com>
2023-11-03 08:11:06 +0700

    Thay doi kich thuoc nut bam

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -95,9 +95,9 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(230, 169);
+            this.button1.Location = new System.Drawing.Point(242, 169);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(85, 29);
+            this.button1.Size = new System.Drawing.Size(73, 29);
             this.button1.TabIndex = 6;
             this.button1.Text = "Cộng";
             this.button1.UseVisualStyleBackColor = true;
@@ -108,7 +108,7 @@
             this.btTru.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btTru.Location = new System.Drawing.Point(115, 169);
             this.btTru.Name = "btTru";
-            this.btTru.Size = new System.Drawing.Size(85, 29);
+            this.btTru.Size = new System.Drawing.Size(73, 29);
             this.btTru.TabIndex = 7;
             this.btTru.Text = "Trừ";
             this.btTru.UseVisualStyleBackColor = true;
@@ -118,7 +118,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(349, 221);
+            this.ClientSize = new System.Drawing.Size(349, 225);
             this.Controls.Add(this.btTru);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.txtKQ);
